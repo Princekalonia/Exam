46def7c782706f708e55ce55eec4cdecb2e9514e
Princekalonia <princekalonia@gmail.com>
2023-05-05 12:34:12 +0530

    1st Commit

---
+++ Prime.java
@@ -0,0 +1,26 @@
+package assignment_2;
+
+public class Prime {
+
+	    public static void main(String[] args) {
+
+	        int low = 2, high = 20;
+
+	        while (low < high) {
+	            boolean flag = false;
+
+	            for(int i = 2; i <= low/2; ++i) {
+	                if(low % i == 0) {
+	                    flag = true;
+	                    break;
+	                }
+	            }
+
+	            if (!flag && low != 0 && low != 1)
+	                System.out.print(low + " ");
+
+	            ++low;
+	        }
+	    }
+	}
+
